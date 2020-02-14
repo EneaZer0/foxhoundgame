512e77eb7481a49ead16d00566f38fc7907d4c3f
EneaZer0 <miguezsergio22@gmail.com>
2020-02-14 03:58:30 +0000

    Stat implementation task 6

--- THE BIG PROJECT/src/main/java/FoxHoundIO.java
+++ THE BIG PROJECT/src/main/java/FoxHoundIO.java
@@ -4,7 +4,19 @@
  * It contains helper functions for all file input / output
  * related operations such as saving and loading a game.
  */
+
+import java.io.FileWriter;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FoxHoundIO {
+
+  public static boolean saveGame (String[] players, char figure, Path path) {
+    boolean save = true;
+
+    return save;
+  }
 
 
 }
